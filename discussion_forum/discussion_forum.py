0b04d9efabf9cded728dec46422af9c2916ef98c
Alan Boudreault <boudreault.alan@gmail.com>
2014-05-04 20:47:53 -0400

    more investigation on the xblock-discussion

--- discussion_forum/discussion_forum.py
+++ discussion_forum/discussion_forum.py
@@ -19,6 +19,35 @@ from .utils import load_resource, render_template, get_scenarios_from_path
 
 log = logging.getLogger(__name__)
 
+# temporary, really...
+JS = [
+'public/js/models/discussion_user.js',
+'public/js/content.js',
+'public/js/discussion.js',
+'public/js/main.js',
+'public/js/views/response_comment_view.js',
+'public/js/views/thread_response_show_view.js',
+'public/js/views/discussion_user_profile_view.js',
+'public/js/views/new_post_inline_vew.js',
+'public/js/views/thread_response_edit_view.js',
+'public/js/views/discussion_thread_view_inline.js',
+'public/js/views/thread_response_view.js',
+'public/js/views/discussion_thread_view.js',
+'public/js/views/discussion_thread_list_view.js',
+'public/js/views/discussion_thread_show_view.js',
+'public/js/views/discussion_thread_edit_view.js',
+'public/js/views/discussion_content_view.js',
+'public/js/views/response_comment_show_view.js',
+'public/js/views/discussion_thread_profile_view.js',
+'public/js/views/new_post_view.js',
+'public/js/views/response_comment_edit_view.js',
+'public/js/discussion_router.js',
+'public/js/utils.js',
+'public/js/templates.js',
+'public/js/discussion_module_view.js',
+'public/js/discussion_filter.js',
+#'public/js/tooltip_manager.js'
+]
 
 # Classes ###########################################################
 
@@ -50,19 +79,41 @@ class DiscussionXBlock(XBlock):
     sort_key = String(scope=Scope.settings)
 
     def student_view(self, context=None):
-        fragment = Fragment(render_template('templates/html/discussion.html', {
+        fragment = Fragment()
+        fragment.add_content(render_template('templates/html/discussion.html', {
             'discussion_id': self.discussion_id
         }))
 
         fragment.add_css_url(self.runtime.local_resource_url(
             self,
-            'public/css/discussion-old.css'
-        ))
-
-        fragment.add_css_url(self.runtime.local_resource_url(
-            self,
             'public/css/discussions-inline.css'
         ))
+
+        fragment.add_javascript_url(
+            self.runtime.local_resource_url(self, 'public/js/vendor/URI-min.js')
+        )
+        fragment.add_javascript_url(
+            self.runtime.local_resource_url(self, 'public/js/vendor/jquery-leanModal-min.js')
+        )
+        fragment.add_javascript_url(
+            self.runtime.local_resource_url(self, 'public/js/vendor/underscore-min.js')
+        )
+        fragment.add_javascript_url(
+            self.runtime.local_resource_url(self, 'public/js/vendor/backbone-min.js')
+        )
+        fragment.add_javascript_url(
+            self.runtime.local_resource_url(self, 'public/js/vendor/mustache.js')
+        )
+
+        for js in JS:
+            fragment.add_javascript_url(self.runtime.local_resource_url(self, js))
+
+        fragment.add_javascript(render_template('public/js/discussion_block.js', {
+            'course_id': self.xmodule_runtime.course_id
+        }))
+
+        fragment.initialize_js('DiscussionBlock')
+
         return fragment
 
     def studio_view(self, context):
